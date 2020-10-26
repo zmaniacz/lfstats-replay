ccd38a4373e5e466111b915996e9349f5c83c689
Russell Lewis <russell.lewis@outlook.com>
2020-10-25 17:11:52 -0700

    install vercel node types

--- api/generate.ts
+++ api/generate.ts
@@ -1,4 +1,6 @@
-module.exports = (req, res) => {
+import { NowRequest, NowResponse } from "@vercel/node";
+
+module.exports = (req: NowRequest, res: NowResponse) => {
   const { name = "World" } = req.query;
   res.send(`Hello ${name}!`);
 };
