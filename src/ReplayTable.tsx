733034d7204b44a409e6e111ccdcba9c049b687d
Russell Lewis <russell.lewis@outlook.com>
2020-12-22 00:41:34 -0800

    DOM fix

--- src/ReplayTable.tsx
+++ src/ReplayTable.tsx
@@ -3,22 +3,26 @@ import React from "react";
 export default function ReplayTable({ state }: any) {
   return (
     <table>
-      <tr>
-        <th>player</th>
-        <th>score</th>
-        <th>shots</th>
-        <th>lives</th>
-      </tr>
-      {Object.values(state).map((element: any) => (
-        <tr key={element.iplId}>
-          <td>
-            {element.name} ({element.position})
-          </td>
-          <td>{element.score}</td>
-          <td>{element.shotsLeft}</td>
-          <td>{element.livesLeft}</td>
+      <thead>
+        <tr>
+          <th>player</th>
+          <th>score</th>
+          <th>shots</th>
+          <th>lives</th>
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        {Object.values(state).map((element: any) => (
+          <tr key={element.iplId}>
+            <td>
+              {element.name} ({element.position})
+            </td>
+            <td>{element.score}</td>
+            <td>{element.shotsLeft}</td>
+            <td>{element.livesLeft}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
