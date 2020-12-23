798148dd14b1b4491e98ea6b29a42303d60d01a9
Russell Lewis <russell.lewis@outlook.com>
2020-12-23 15:30:23 -0800

    update spinner

--- src/LFLoad.tsx
+++ src/LFLoad.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { EuiLoadingSpinner } from "@elastic/eui";
 
 export function LoadError() {
   return <div>error</div>;
 }
 
 export function LoadSpinner() {
-  return <div>loading...</div>;
+  return <EuiLoadingSpinner size="xl" />;
 }
